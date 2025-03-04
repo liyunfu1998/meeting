973f088e95feab1363e83378dcc77605da0ae475
liyunfu1998 <liyunfu1998@qq.com>
2025-03-04 15:30:10 +0800

    feat: 定义数据表

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
+import * as fs from 'fs';
 
 @Module({
   imports: [
@@ -14,13 +19,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'fortune500', // 要连接的数据库名称
       synchronize: true, // 自动同步实体到数据库表结构
       logging: true, // 启用SQL查询日志记录
-      entities: [], // 实体类数组，用于ORM映射
+      entities: [User, Role, Permission], // 实体类数组，用于ORM映射
       poolSize: 10, // 连接池大小设置
       connectorPackage: 'mysql2', // 使用mysql2作为连接器
       extra: {
         authPlugins: 'sha256_password', // 使用sha256_password认证插件
       },
+      ssl: {
+        ca: fs.readFileSync('/etc/ssl/cert.pem'),
+      },
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
