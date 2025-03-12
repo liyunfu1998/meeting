28de07cb49cac589b2878da20f9d39ab97fe69e8
liyunfu1998 <liyunfu1998@qq.com>
2025-03-12 17:38:28 +0800

    feat: 错误返回格式统一

---
+++ server/src/custom-exception.filter.ts
@@ -0,0 +1,25 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class CustomExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse<Response>();
+    response.statusCode = exception.getStatus();
+
+    const res = exception.getResponse() as { message: string[] };
+
+    response
+      .json({
+        code: exception.getStatus(),
+        menubar: 'fail',
+        data: res?.message?.join(',') || exception.message,
+      })
+      .end();
+  }
+}
